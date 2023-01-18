aa19986c1f2c596c2615d76d3fd368b2800eec2f
Md-Alqma <mdalqma7@gmail.com>
2023-01-19 00:38:24 +0530

    ADD: details page

---
+++ src/components/Detail.js
@@ -0,0 +1,141 @@
+import React from "react";
+import styled from "styled-components";
+const Detail = () => {
+  return (
+    <Container>
+      <Background>
+        <img
+          src="https://static-koimoi.akamaized.net/wp-content/new-galleries/2015/08/talvar-movie-poster-2.jpg"
+          alt="background"
+        />
+      </Background>
+      <ImageTitle>
+        <img src="/details/detail.png" alt="title" />
+      </ImageTitle>
+      \
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" alt="play button" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" alt="play button" />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" alt="group icon" />
+        </GroupWatchButton>
+      </Controls>
+      <SubTitle>2018 - 7m - Family, Fantasy, Kids, Animation</SubTitle>
+      <Description>
+        Find & Download Free Graphic Resources for Movie Poster. 9000+ Vectors,
+        Stock Photos & PSD files. ✓ Free for commercial use ✓ High Quality
+        Images
+      </Description>
+    </Container>
+  );
+};
+
+export default Detail;
+const Container = styled.div`
+  min-height: calc(100vh-70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vw;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background: rgb(249, 249, 249);
+  border: none;
+  padding: 0px 24px;
+  margin-right: 22px;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
+`;
