3a98ad9e4fe1030044f9323bb3c66a444e1f4ee4
Md-Alqma <mdalqma7@gmail.com>
2023-01-20 01:47:07 +0530

    ADD: firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
 import Movies from "./Movies";
 import Viewers from "./Viewers";
 import Footer from "./Footer";
 // import db from "../firebase";
+import { db } from "../firebase";
+import app from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const colRef = collection(db, "movies");
   // useEffect(() => {
-  //   db.collection("movies").onSnapShot((snapshot) => {
+  //   getDoc(colRef).then((snapshot) => {
   //     console.log(snapshot);
   //   });
   // }, []);
+
+  useEffect(() => {
+    getDocs(colRef).then((snapshot) => {
+      let moviesArr = snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      setMovies(moviesArr);
+    });
+  }, []);
+  console.log(movies);
   return (
     <Container>
       <ImageSlider />
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,45 @@
-import firebase from 'firabase'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-  projectid: "disneyplus-clone-a33d5",
-  storageBucket: "disneyplus-clone-a33d5.appspot.com",
-  messagingSenderId: "37918794208",
-  appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-  measurementId: "G-DRVLJKWRWG",
+  apiKey: "AIzaSyCNwpcTV2NKIuyecbS0BFtCCaOuMGL3i3g",
+  authDomain: "disney-plus-f7b33.firebaseapp.com",
+  projectId: "disney-plus-f7b33",
+  storageBucket: "disney-plus-f7b33.appspot.com",
+  messagingSenderId: "518382556906",
+  appId: "1:518382556906:web:314706a9db549663e66993",
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebase.storage();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
 
-export { auth, provider, storage };
-export default db;
+export default app;
+
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
+// import "firebase/compat/storage";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
+//   authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
+//   projectId: "disneyplus-clone-a33d5",
+//   storageBucket: "disneyplus-clone-a33d5.appspot.com",
+//   messagingSenderId: "37918794208",
+//   appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
+//   measurementId: "G-DRVLJKWRWG",
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// const provider = new firebase.auth.GoogleAuthProvider();
+// const storage = firebase.storage();
+
+// export { auth, provider, storage };
+// export default db;
